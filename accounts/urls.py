c560bf66ec50003d28cfebaa5f99e1682aeb1dc5
mpd00 <parali68@gmail.com>
2022-03-07 17:56:25 +0330

    Create Sign Up API
    Created view, serializer for Sign Up API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
+from .views import SignUpAPI
 from django.urls import path, include
 from knox import views as knox_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
+    path('signup/', SignUpAPI.as_view(), name='signup'),
 ]

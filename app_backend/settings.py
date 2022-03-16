4586fe30f56c58a9c2f15345656f34678a9366db
mpd00 <parali68@gmail.com>
2022-03-16 18:46:13 +0330

    Create Reset Password API

--- app_backend/settings.py
+++ app_backend/settings.py
@@ -28,7 +28,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = 'django-insecure-x^#bitj(cd!1*jfsh))o9fn^$rsrrz$hdkmmxkuz(v8g819ab9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['amoozande.herokuapp.com','127.0.0.1']
 
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     'accounts',
+    'django_rest_passwordreset',
+
 ]
 
 MIDDLEWARE = [

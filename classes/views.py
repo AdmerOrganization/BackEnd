ddcff34fc6cac5b025856a2fd1929ccc06a6016b
mpd00 <parali68@gmail.com>
2022-04-07 11:28:16 +0430

    Add classroom serializer and view's structure

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,17 @@
+import imp
 from django.shortcuts import render
-
+from rest_framework import generics, status
+from serializers import Classroom_SearchSerializer
+from django.db import connection
 # Create your views here.
+
+
+class Classroom_SearchAPI(generics.GenericAPIView):
+    serializer_class = Classroom_SearchSerializer
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+
+        
+        return Response(serializer.data)
